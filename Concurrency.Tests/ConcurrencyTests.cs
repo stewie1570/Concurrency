8e9ed43eb0896eab5f766323eda5b8de55bd19c3
stewie1570 <stewie1570@gmail.com>
2018-01-12 11:34:55 -0700

    Forgot to add a test to assert no-repeating tasks.

--- Concurrency.Tests/ConcurrencyTests.cs
+++ Concurrency.Tests/ConcurrencyTests.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Threading.Tasks;
+using System;
 using System.Collections.Generic;
-using FluentAssertions;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Concurrency.Tests
 {
@@ -93,6 +93,52 @@ namespace Concurrency.Tests
                 .Max(log => log.NumTasksRunning)
                 .Should()
                 .Be(maxConcurrency, because: $"there should be up to {maxConcurrency} tasks in flight.");
+        }
+
+        [TestMethod]
+        public async Task ShouldNotRepeatTasks()
+        {
+            //Arrange
+            var logs = new List<Log>();
+            var numTasksRunning = 0;
+            var taskCompletions = Enumerable
+                .Range(start: 1, count: 10)
+                .Select(i => new TaskCompletionSource<int>())
+                .ToList();
+            Func<int, Task> addLog = async taskId =>
+            {
+                numTasksRunning++;
+                logs.Add(new Log { Id = taskId, NumTasksRunning = numTasksRunning });
+                await taskCompletions[taskId - 1].Task;
+                numTasksRunning--;
+            };
+
+            var tasks = new List<Func<Task>>
+            {
+                async () => await addLog(1),
+                async () => await addLog(2),
+                async () => await addLog(3),
+                async () => await addLog(4),
+                async () => await addLog(5),
+                async () => await addLog(6),
+                async () => await addLog(7),
+                async () => await addLog(8),
+                async () => await addLog(9),
+                async () => await addLog(10)
+            };
+
+            //Act
+            int maxConcurrency = 3;
+            var whenAllComplete = Concurrency.WhenAll(tasks, maxConcurrency);
+            taskCompletions.ForEach(taskCompletion => taskCompletion.SetResult(0));
+            await whenAllComplete;
+
+            //Assert
+            var repeats = logs
+                .Select(log => new { Id = log.Id, Count = logs.Count(l => l.Id == log.Id) })
+                .Where(result => result.Count > 1)
+                .ToList();
+            repeats.Count.Should().Be(0, because: $"there should be no repeats but: {string.Join(", ", repeats.Select(repeat => $"(Id: {repeat.Id}, Count: {repeat.Count})"))}.");
         }
     }
 }
